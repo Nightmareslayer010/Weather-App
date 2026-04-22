ca1bd2b2d037efb5d9d932c8f5eb6bb86eff072b
Giriraj <girirajvyas010@gmail.com>
2026-04-22 19:56:28 +0530

    Search Validation V1

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 let cityInput = document.querySelector("#cityInput");
 let searchBtn = document.querySelector("#searchBtn");
 let result = document.querySelector("#result");
+
+// functions
+
+function searchHandler() {
+  let city = cityInput.value.trim();
+  if (city === "") {
+    cityInput.value = "";
+    cityInput.placeholder = " Please Enter a Valid City";
+    cityInput.classList.add("emptyCity");
+    result.textContent = "";
+    return;
+  }
+  cityInput.classList.remove("emptyCity");
+  cityInput.placeholder = "Enter City Name";
+  result.textContent = city;
+}
+
+//event handler
+
+searchBtn.addEventListener("click", searchHandler);
+
+cityInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    searchHandler();
+  }
+});
